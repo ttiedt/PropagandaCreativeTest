314ce76d2bb50154aa70248ffd760308108105aa
ttiedt <ttiedt@zagmail.gonzaga.edu>
2020-07-28 20:10:16 -0700

    comment dropdown.js

--- scripts/dropdownnav.js
+++ scripts/dropdownnav.js
@@ -1,5 +1,8 @@
+// This function displays the nav links
+// when hamburger button is clicked
 function dropDown() {
   var x = document.getElementById("nav-links");
+  // onClick it changes the 'nav-links' to displayed or hidden.
   if(x.style.display === "block") {
     x.style.display = "none";
   } else {
